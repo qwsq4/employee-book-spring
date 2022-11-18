a3cc274381a1471d56194670fd93d83b6780b63b
qwsq4 <qwsq1437@gmail.com>
2022-11-18 19:48:25 +0800

    Initial commit

---
+++ src/main/java/com/skypro/employeebookspring/controller/EmployeeController.java
@@ -0,0 +1,50 @@
+package com.skypro.employeebookspring.controller;
+
+import com.skypro.employeebookspring.model.Employee;
+import com.skypro.employeebookspring.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.skypro.employeebookspring.record.EmployeeRequest;
+
+import java.util.Collection;
+
+@RestController
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public Collection<Employee> getAllEmployees() {
+        return this.employeeService.getAllEmployees();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody EmployeeRequest employeeRequest) {
+        return this.employeeService.addEmployee(employeeRequest);
+    }
+
+    @GetMapping("/employee/salary/sum")
+    public int getSalarySum() {
+        return this.employeeService.getSalarySum();
+    }
+
+    @GetMapping("/employee/salary/min")
+    public Employee getSalaryMin() {
+        return this.employeeService.getSalaryMin();
+    }
+
+    @GetMapping("/employee/salary/max")
+    public Employee getSalaryMax() {
+        return this.employeeService.getSalaryMax();
+    }
+
+    @GetMapping("/employee/high-salary")
+    public Collection<Employee> getSalaryHigh() {
+        return this.employeeService.getSalaryHigh();
+    }
+}
---
+++ src/main/java/com/skypro/employeebookspring/service/EmployeeService.java
@@ -0,0 +1,76 @@
+package com.skypro.employeebookspring.service;
+
+import com.skypro.employeebookspring.model.Employee;
+import org.springframework.stereotype.Service;
+import com.skypro.employeebookspring.record.EmployeeRequest;
+
+import java.util.*;
+
+@Service
+public class EmployeeService {
+    private final Map<Integer, Employee> employees = new HashMap<>();
+
+    public Collection<Employee> getAllEmployees() {
+        return this.employees.values();
+    }
+
+    public Employee addEmployee(EmployeeRequest employeeRequest) {
+        if (employeeRequest.getFirstName() == null || employeeRequest.getLastName() == null) {
+            throw new IllegalArgumentException("Имя работника должно быть указано");
+        }
+
+        Employee employee = new Employee(employeeRequest.getFirstName(),
+                employeeRequest.getLastName(),
+                employeeRequest.getDepartment(),
+                employeeRequest.getSalary());
+
+        this.employees.put(employee.getId(), employee);
+        return employee;
+    }
+
+    public int getSalarySum() {
+        return employees.values()
+                .stream()
+                .mapToInt(Employee::getSalary)
+                .sum();
+    }
+
+    public Employee getSalaryMin() {
+        return employees.values()
+                .stream()
+                .min(salaryComparator)
+                .get();
+    }
+
+    public Employee getSalaryMax() {
+        return employees.values()
+                .stream()
+                .max(salaryComparator)
+                .get();
+    }
+
+    public double getSalaryAverage() {
+        return employees.values()
+                .stream()
+                .mapToInt(Employee::getSalary)
+                .average()
+                .getAsDouble();
+    }
+
+    public Collection<Employee> getSalaryHigh() {
+        Map<Integer, Employee> highSalaryEmployees = new HashMap<>();
+        for (int i = 0; i < employees.size(); i++) {
+            if (employees.get(i).getSalary() > getSalaryAverage()) {
+                highSalaryEmployees.put(employees.get(i).getId(), employees.get(i));
+            }
+        }
+        return highSalaryEmployees.values();
+    }
+
+    Comparator<Employee> salaryComparator = new Comparator<Employee>() {
+        @Override
+        public int compare(Employee o1, Employee o2) {
+            return Integer.compare(o1.getSalary(), o2.getSalary());
+        }
+    };
+}
